ffe1ec81a15b02df28b21ce3d0429dfef2dee1b4
Zach <95zmartin@gmail.com>
2019-01-27 18:12:30 -0500

    linked spotify API keys to liri javascript file

--- liri.js
+++ liri.js
@@ -1,6 +1,9 @@
 // Reads and sets any environmental variables with the dotenv package
-require("dotenv").config();
+require('dotenv').config();
 
-var keys = require("./keys.js");
+var keys = require('./keys.js');
+var spotifyAPI = require('node-spotify-api');
 
-console.log(keys);
+var spotify = new spotifyAPI(keys.spotify);
+
+console.log(spotify);
